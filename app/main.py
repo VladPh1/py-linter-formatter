06b6e2d21a5be4c011eec96e2465da25569d2bca
Vladyslav Pihol <freepgvlad@gmail.com>
2025-02-11 11:45:53 +0200

    Test3

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
+from pyexpat.errors import messages
+
+
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    return {**{mapping[old_error]: value for old_error, value in error.items() if old_error in mapping}, "source": "flake8"}
+    return {**{new_key: error[old_key] for old_key, new_key in {"line_number": "line", "column_number": "column", "text": "message", "code": "name"}.items() if old_key in error}, "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
     return {"errors": [format_linter_error(error) for error in errors], "path": file_path, "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
     return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
